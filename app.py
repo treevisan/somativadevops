23e25a6a4a5ccf76f8e7fe7120e1edb9d495635e
Gabrieli Trevisan <78770342+treevisan@users.noreply.github.com>
2023-04-24 20:58:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/add', methods=['POST'])
-def add():
+
+@app.route('/multiply', methods=['POST'])
+def multiply():
     num1 = int(request.form['num1'])
     num2 = int(request.form['num2'])
-    result = num1 + num2
+    result = num1 * num2
     return render_template('result.html', result=result)
 
 
